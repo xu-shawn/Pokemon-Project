da2fb33bed0d7da1111c07fcf8561b624e371bd4
CarbonIce <seanxiang2019@gmail.com>
2024-03-09 17:59:59 -0800

    minor notes

--- src/pokemon.py
+++ src/pokemon.py
@@ -1,3 +1,6 @@
 class Pokemon:
-    def __init__(self, name, type, ):
-        
+    def __init__(self, name, ttype, health, *attacks):
+        self.name = name
+        self.type = ttype # Not using "type" because its a keyword
+        self.health = health
+        self.attacks = attacks
